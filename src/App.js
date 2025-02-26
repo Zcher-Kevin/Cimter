60fc918a925a2d5f50610d0382f4a84ebf52f54e
Chan Wai Lung <chan0629w@gmail.com>
2025-02-26 23:28:19 +0800

    version 0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Clock from './pages/Clock';
+import Stopwatch from './pages/Stopwatch';
+import Timer from './pages/Timer';
+import Button from './components/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+        <div className="App">
+            <div className = "Menu">
+                <Button to="/">Home</Button>
+                <Button to="/clock">Clock</Button>
+                <Button to="/stopwatch">Stopwatch</Button>
+                <Button to="/timer">Timer</Button>
+            </div>
+            <div className = "Page">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/stopwatch" element={<Stopwatch />} />
+                    <Route path="/timer" element={<Timer />} />
+                    <Route path="/clock" element={<Clock />} />
+                </Routes>
+            </div>
+        </div>
+    </Router>
+  )
 }
 
 export default App;

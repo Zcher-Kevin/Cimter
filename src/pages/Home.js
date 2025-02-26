d174c8b9cf1bacf256f796bcecff3db4b8bd4110
Chan Wai Lung <chan0629w@gmail.com>
2025-02-26 23:15:46 +0800

    Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Css/Home.css";
+
+function Home() {
+    return (
+        <div className='Home'>
+            <h1>
+            This is Home page
+            </h1>
+        </div>
+    )
+}
+    
+export default Home

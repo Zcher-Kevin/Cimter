735496e1c421900a5f16a5675f2e873099b5e5c7
Chan Wai Lung <chan0629w@gmail.com>
2025-03-20 21:32:45 +0800

    include new function

--- src/App.js
+++ src/App.js
@@ -6,26 +6,31 @@ import Clock from './pages/Clock';
 import Stopwatch from './pages/Stopwatch';
 import Timer from './pages/Timer';
 import Button from './components/Button';
-
+import { TimerProvider } from './context/TimerContext';
+import { TimerCountProvider } from './context/TimerCountContext';
 function App() {
   return (
     <Router>
-        <div className="App">
-            <div className = "Menu">
-                <Button to="/">Home</Button>
-                <Button to="/clock">Clock</Button>
-                <Button to="/stopwatch">Stopwatch</Button>
-                <Button to="/timer">Timer</Button>
+        <TimerProvider>
+            <TimerCountProvider>
+            <div className="App">
+                <div className = "Menu">
+                    <Button to="/">Home</Button>
+                    <Button to="/clock">Clock</Button>
+                    <Button to="/stopwatch">Stopwatch</Button>
+                    <Button to="/timer">Timer</Button>
+                </div>
+                <div className = "Page">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/stopwatch" element={<Stopwatch />} />
+                        <Route path="/timer" element={<Timer />} />
+                        <Route path="/clock" element={<Clock />} />
+                    </Routes>
+                </div>
             </div>
-            <div className = "Page">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/stopwatch" element={<Stopwatch />} />
-                    <Route path="/timer" element={<Timer />} />
-                    <Route path="/clock" element={<Clock />} />
-                </Routes>
-            </div>
-        </div>
+            </TimerCountProvider>
+        </TimerProvider>
     </Router>
   )
 }
